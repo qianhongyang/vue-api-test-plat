8d60f117698f9a79414a5b7920c9f2f892a2db29
dean.qian <dean.qian@rccchina.com>
2020-05-14 16:31:45 +0800

    增加收起时去掉下符号

--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -9,7 +9,7 @@
     .layout-logo {
         width: 100px;
         height: 30px;
-        background: #5b6270;
+        background: url(/src/assets/images/error-page/cute_girl.jpeg)no-repeat center center fixed;;
         border-radius: 3px;
         float: left;
         position: relative;
@@ -25,7 +25,7 @@
         height: 100%;
         width: 100%;
     }
-    .menu-item span{
+    .menu-item   span{
         display: inline-block;
         overflow: hidden;
         width: 69px;
@@ -34,29 +34,35 @@
         vertical-align: bottom;
         transition: width .2s ease .2s;
     }
-    .menu-item i{
+    .menu-item  i{
         transform: translateX(0px);
         transition: font-size .2s ease, transform .2s ease;
         vertical-align: middle;
         font-size: 16px;
     }
-    .collapsed-menu span{
+    .collapsed-menu  span{
         width: 0px;
         transition: width .2s ease;
     }
-    .collapsed-menu i{
+    .collapsed-menu  i{
         transform: translateX(5px);
         transition: font-size .2s ease .2s, transform .2s ease .2s;
         vertical-align: middle;
         font-size: 22px;
     }
+    .collapsed-menu /deep/ .ivu-icon-ios-arrow-down{
+        display: none;
+    }
+
 </style>
 <template>
     <div class="layout">
         <Layout>
             <Header>
                 <Menu mode="horizontal" theme="dark" active-name="1">
-                    <div class="layout-logo"></div>
+                    <div class="layout-logo">
+
+                    </div>
                     <div class="layout-nav">
                         <MenuItem name="1">
                             <Icon type="ios-navigate"></Icon>
@@ -86,8 +92,6 @@
                                 <span>Item 1</span>
                             </template>
                             <MenuItem name="1-1">Option 1</MenuItem>
-                            <MenuItem name="1-2">Option 2</MenuItem>
-                            <MenuItem name="1-3">Option 3</MenuItem>
                         </Submenu>
                         <Submenu name="2">
                             <template slot="title">
@@ -95,18 +99,16 @@
                                 <span>Item 2</span>
                             </template>
                             <MenuItem name="2-1">Option 1</MenuItem>
-                            <MenuItem name="2-2">Option 2</MenuItem>
                         </Submenu>
                         <Submenu name="3">
                             <template slot="title">
                                 <Icon type="ios-analytics"></Icon>
                                 <span>Item 3</span>
                             </template>
-                            <MenuItem name="3-1">
+                            <MenuItem class="layout-con" name="3-1">
                                 <Icon type="ios-analytics"></Icon>
                                 <span>Option 1</span>
                             </MenuItem>
-                            <MenuItem name="3-2">Option 2</MenuItem>
                         </Submenu>
                     </Menu>
                 </Sider>
